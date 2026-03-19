5f735ac1848ca12d6f65477eca95ab3780acdc10
Gowtham <gowthambabu8@gmail.com>
2026-03-19 19:57:36 +0530

    terraform backend module changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,4 +8,4 @@ cd /home/ec2-user/
 git clone https://github.com/gowthambabu8/roboshop-ansible-roles-tf.git
 cd roboshop-ansible-roles-tf
 git pull
-ansible-playbook -e component=$component -e environment=$env -e app_version=$app_version roboshop.yaml
+ansible-playbook -e component=$component -e env=$env -e app_version=$app_version roboshop.yaml

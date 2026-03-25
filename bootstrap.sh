d919a9446574f1081f25ccc34f1ca56e93730bcc
Gowtham <gowthambabu8@gmail.com>
2026-03-25 21:30:29 +0530

    terraform aws sg changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,5 +9,6 @@ domain_name=$4
 cd /home/ec2-user/
 git clone https://github.com/gowthambabu8/roboshop-ansible-roles-tf.git
 cd roboshop-ansible-roles-tf
+git stash
 git pull
 ansible-playbook -e component=$component -e env=$env -e app_version=$app_version -e domain_name=$domain_name roboshop.yaml
